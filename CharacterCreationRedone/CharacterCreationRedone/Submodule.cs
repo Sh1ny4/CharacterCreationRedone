81c2168968e13ce012e9e51e3d7226e1f29daf23
Sh1ny4 <60614398+Sh1ny4@users.noreply.github.com>
2025-09-17 19:10:46 +0200

    Initial commit

---
+++ CharacterCreationRedone/CharacterCreationRedone/Submodule.cs
@@ -0,0 +1,19 @@
+﻿
+
+using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace CharacterCreationRedone
+{
+    public class Submodule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            new Harmony("CharacterCreationRedone.CharacterCreationOptions").PatchAll();
+        }
+    }
+}
